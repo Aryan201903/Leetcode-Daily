2d5b72fc46f1bd6a77a3b9a443df8094e13e28a1
Aryan Garg <102370119+Aryan201903@users.noreply.github.com>
2024-07-07 20:47:16 +0530

    Create 1518.java

---
+++ 1518.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int sum = numBottles;
+        while (numBottles >= numExchange){
+            int mod = numBottles % numExchange;
+            numBottles/=numExchange;
+            sum+=numBottles;
+            numBottles+=mod;
+        }
+        return sum;
+    }   
+}
