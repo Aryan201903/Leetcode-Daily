9ccf11b30f38ce62e3ddc3ace6bc55234453379e
Aryan Garg <102370119+Aryan201903@users.noreply.github.com>
2024-08-05 11:30:02 +0530

    Create 2053.java

---
+++ 2053.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String a : arr){
+            map.put(a , map.getOrDefault(a,0) + 1);
+        }   
+
+        for(String v : arr){
+            if(map.get(v) == 1){
+                --k;
+                if(k == 0) return v;
+            }
+        }
+        return "";
+    }
+}
